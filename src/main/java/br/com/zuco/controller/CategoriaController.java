e0d94d0b2708bc1ccb1770691c208fadb69cfb69
zucoloto <zucoloto.alessandro@gmail.com>
2025-09-23 14:51:12 -0300

    Salvar uma lista de Categorias em uma única requisição

--- src/main/java/br/com/zuco/controller/CategoriaController.java
+++ src/main/java/br/com/zuco/controller/CategoriaController.java
@@ -1,9 +1,14 @@
 package br.com.zuco.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zuco.entity.Categoria;
 import br.com.zuco.service.CategoriaService;
 
 @RestController
@@ -12,5 +17,10 @@ public class CategoriaController {
 
 	@Autowired
 	private CategoriaService categoriaService;
+	
+	@PostMapping
+    public List<Categoria> salvar(@RequestBody List<Categoria> categorias) {
+        return categoriaService.salvar(categorias);
+    }
 	
 }
--- src/main/java/br/com/zuco/service/CategoriaService.java
+++ src/main/java/br/com/zuco/service/CategoriaService.java
@@ -1,9 +1,13 @@
 package br.com.zuco.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.zuco.entity.Categoria;
 import br.com.zuco.repository.CategoriaRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class CategoriaService {
@@ -11,4 +15,8 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Transactional
+    public List<Categoria> salvar(List<Categoria> categorias) {
+        return categoriaRepository.saveAll(categorias);
+    }
 }
