16b974c130abea2a9b31d9e4b267fd0645799648
zucoloto <zucoloto.alessandro@gmail.com>
2025-09-23 19:34:41 -0300

    Inserir Posts

--- src/main/java/br/com/zuco/controller/PostController.java
+++ src/main/java/br/com/zuco/controller/PostController.java
@@ -1,9 +1,12 @@
 package br.com.zuco.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zuco.entity.Post;
 import br.com.zuco.service.PostService;
 
 @RestController
@@ -12,5 +15,10 @@ public class PostController {
 
 	@Autowired
     private PostService postService;
+	
+	@PostMapping
+    public Post salvar(@RequestBody Post post) {
+        return postService.salvar(post);
+    }
 	
 }
--- src/main/java/br/com/zuco/service/PostService.java
+++ src/main/java/br/com/zuco/service/PostService.java
@@ -1,8 +1,14 @@
 package br.com.zuco.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.zuco.entity.Autor;
+import br.com.zuco.entity.Categoria;
+import br.com.zuco.entity.Post;
 import br.com.zuco.repository.PostRepository;
 
 @Service
@@ -10,5 +16,23 @@ public class PostService {
 
 	@Autowired
     private PostRepository postRepository;
+	
+	@Autowired
+    private AutorService autorService;
     
+	@Autowired
+    private CategoriaService categoriaService;
+    
+	@Transactional
+    public Post salvar(Post post) {
+        Autor autor = autorService.buscarPorId(post.getAutor().getAutorId());
+        post.setAutor(autor);
+
+        List<String> titulos = post.getCategorias().stream().map(Categoria::getCategoriaTitulo).toList();
+        List<Categoria> categorias = categoriaService.buscarPorTitulos(titulos);
+        post.setCategorias(categorias);
+
+        return this.postRepository.save(post);
+    }
+	
 }
